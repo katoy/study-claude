1f62df3a335cead4a9bcd72feae7e340ebeade4b
katoy <youichikato@gmail.com>
2026-07-20 22:25:54 +0900

    chore: run lint before commits

---
+++ contact-form/myproject/.remarkrc.mjs
@@ -0,0 +1,6 @@
+import remarkGfm from "remark-gfm";
+import remarkPresetLintRecommended from "remark-preset-lint-recommended";
+
+export default {
+  plugins: [remarkGfm, remarkPresetLintRecommended],
+};
